65627d02eabcd12276713133c6aaf4fa08cc27c0
Kousuke Nagatsuka <koron393@gmail.com>
2015-02-01 22:11:37 +0900

    SEPolicy: Fix selinux denials

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -133,8 +133,9 @@ BOARD_SEPOLICY_UNION += \
         bluetooth_loader.te \
         conn_init.te \
         hostapd.te \
-        tee.te \
-        sysinit.te
+        su.te \
+        sysinit.te \
+        tee.te
 
 # Enable Minikin text layout engine (will be the default soon)
 USE_MINIKIN := true
