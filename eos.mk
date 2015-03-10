e91390baaa359af353ca7c8c79eabce4a604d5d3
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-03-10 08:15:55 +0000

    Flo: build TouchControl
    Change-Id: I5364e0c62a915d1e05fd726c7c94dd49bf19006f

--- eos.mk
+++ eos.mk
@@ -27,6 +27,10 @@ TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razor BUILD_FINGERPRINT=google/razor/flo:5.0.2/LRX22G/1649326:user/release-keys PRIVATE_BUILD_DESC="razor-user 5.0.2 LRX22G 1649326 release-keys"
 
+# TouchControl package.
+PRODUCT_PACKAGES += \
+	TouchControl
+
 # Copy device specific prebuilt files.
 PRODUCT_COPY_FILES += \
     vendor/eos/prebuilt/common/bootanimations/BOOTANIMATION-1080x1920.zip:system/media/bootanimation.zip
